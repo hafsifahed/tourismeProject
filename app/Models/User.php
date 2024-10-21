0518c45e0934f710619fcdd9db1bd57c193d8ace
AzizSouissi <aziz.souissi01@gmail.com>
2024-10-21 20:19:52 +0100

    fixed User.php mistake

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,61 +1,71 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use App\Models\GuideLocal;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\ReservationTour;
+use App\Models\AvisTour;
+use App\Models\TypeTour;
 
-class GuideLocalController extends Controller
+class User extends Model
 {
-    // Retrieve all guides
-    public function index()
+    use HasFactory;
+
+    protected $table = 'users';
+    protected $fillable = [
+        'username',
+        'firstname',
+        'lastname',
+        'email',
+        'password',
+        'address',
+        'city',
+        'country',
+        'postal',
+        'about'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Always encrypt the password when it is updated.
+     *
+     * @param $value
+     * @return string
+     */
+    public function setPasswordAttribute($value)
     {
-        return GuideLocal::all();
+        $this->attributes['password'] = bcrypt($value);
     }
 
-    // Create a new guide
-    public function store(Request $request)
+    public function typeTour()
     {
-        $validatedData = $request->validate([
-            'nom' => 'required|string|max:255',
-            'type_tour' => 'required|exists:types_tours,id',
-            'description' => 'nullable|string',
-            'region' => 'nullable|string',
-            'ville' => 'nullable|string',
-            'disponibilites' => 'nullable|string',
-            'telephone' => 'nullable|string|max:20',
-            'email' => 'nullable|email|max:100',
-            'site_web' => 'nullable|url',
-            'certification' => 'boolean',
-            'tour_groupe' => 'boolean',
-            'tour_prive' => 'boolean',
-            'photo_url' => 'nullable|string',
-        ]);
-
-        $guide = GuideLocal::create($validatedData);
-        return response()->json($guide, 201);
+        return $this->belongsTo(TypeTour::class, 'type_tour');
     }
 
-    // Retrieve a specific guide by ID
-    public function show($id)
-    {
-        $guide = GuideLocal::findOrFail($id);
-        return response()->json($guide);
+    public function reservationsTours() {
+        return $this->hasMany(ReservationTour::class, 'utilisateur');
     }
 
-    // Update a guide
-    public function update(Request $request, $id)
-    {
-        $guide = GuideLocal::findOrFail($id);
-        $guide->update($request->all());
-        return response()->json($guide, 200);
-    }
-
-    // Delete a guide
-    public function destroy($id)
-    {
-        $guide = GuideLocal::findOrFail($id);
-        $guide->delete();
-        return response()->json(null, 204);
+    public function avisTours() {
+        return $this->hasMany(AvisTour::class, 'utilisateur');
     }
 }
